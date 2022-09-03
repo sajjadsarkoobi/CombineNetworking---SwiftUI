12d30367c357488cf5d74500673b0d0382b1db1c
Sajjad Sarkoobi <sajjadsarkoobi@gmail.com>
2022-09-03 16:42:10 +0300

    Initial Commit

---
+++ SampleCombineNetworking/SampleCombineNetworkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleCombineNetworkingApp.swift
+//  SampleCombineNetworking
+//
+//  Created by Sajjad Sarkoobi on 3.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct SampleCombineNetworkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
